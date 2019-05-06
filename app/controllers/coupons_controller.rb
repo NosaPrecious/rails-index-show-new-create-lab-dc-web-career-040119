9bba840636c3f2ce9b9135f22887802b45ee1c7a
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-06 03:55:05 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -10,15 +10,16 @@ class CouponsController < ApplicationController
   end
   
   def new
-      @coupon = Coupon.new
+  
   end
   
   def create
-  @coupon = Coupon.new
-    @coupon[coupon_code]= params[:coupon][:coupon_code]
-    @coupon[store]= params[:coupon][:store]
+     @coupon = Coupon.new
+    @coupon[:coupon_code] = params[:coupon][:coupon_code]
+    @coupon[:store] = params[:coupon][:store]
     @coupon.save
     redirect_to coupon_path(@coupon)
+    
   end  
   
   def edit
